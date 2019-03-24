1c190d5e47ea192813f51ed15a5cbe4257b5d167
KamataHiromichi <1216kamata@gmail.com>
2019-03-24 18:48:06 +0900

    modify 12

--- set2/c12.py
+++ set2/c12.py
@@ -39,17 +39,27 @@ def detect_cipher_mode(key: str, min_key_length: int = 10, max_key_length=100) -
     return {'mode': 'CBC', 'block_size': None}
 
 
+def get_secret_string_size(secret: bytes, block_size: int) -> int:
+    secret_cipher_length = len(encrypt_ecb(
+        pkcs_7_padding(secret, block_size), KEY))
+    i = 0
+    while True:
+        i += block_size
+        As = b'A'*i
+        as_length = len(encrypt_ecb(As, KEY))
+        if as_length == secret_cipher_length:
+            return i
+
+
 def decrypt_secret_string(secret: bytes):
     ciper_property = detect_cipher_mode(KEY)
     if ciper_property['mode'] != 'ECB':
         return -1
     block_size = ciper_property['block_size']
     secret_pad = pkcs_7_padding(secret, block_size)
-    secret_string_size = len(secret_pad)
+    secret_string_size = get_secret_string_size(secret, block_size)
     As = b'A' * secret_string_size
     guessed = b''
-    start = (secret_string_size // block_size - 1) * block_size
-    stop = start + 16
     old_i = -1
     for i in range(secret_string_size):
         plain_text = As[:-i-1] + secret_pad
